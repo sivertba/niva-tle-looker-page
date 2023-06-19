6a62da62bdc4c8388b374b4cec1fef543b8edb26
sivertba <sivert.bakken@gmail.com>
2023-06-19 11:35:53 +0200

    Fix pass computation

--- pass_computer.py
+++ pass_computer.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from pyorbital.orbital import Orbital
 import requests
+from weather.ccmet import CCMET
+import json
+import os
+import pandas as pd
 
 # Debug flag
 DEBUG = False
@@ -37,7 +41,6 @@ def collect_TLEs(satellites: dict) -> dict:
 
 
 def compute_passes(satellites: dict, locations: dict, look_ahead_time: int = 24*3) -> dict:
-    satellites = collect_TLEs(satellites)
     if DEBUG:
         print("collect_TLEs() successful")
     for satellite in satellites:
@@ -60,11 +63,71 @@ def compute_passes(satellites: dict, locations: dict, look_ahead_time: int = 24*
                 tol=1
             )
 
+            # extract max elevation datetime and compute elevation
+            pass_info = []
+            for i in range(len(loc_info)):
+                pass_info.append(dict())
+                
+                pass_info[i]["UTC0_datetime"] = loc_info[i][2].strftime("%Y-%m-%d %H:%M:%SZ")
 
+                temp_obj = sat_obj.get_observer_look(loc_info[i][2], 
+                                                         locations[loc][0], 
+                                                         locations[loc][1], 
+                                                         locations[loc][2])
+                
+                # reduce to two decimals
+                temp_obj = [round(temp_obj[0], 2), round(temp_obj[1], 2)]
 
-            satellites[satellite][3][loc] = loc_info
+                pass_info[i]["azimuth"] = temp_obj[0]
+                pass_info[i]["elevation"] = temp_obj[1]
+
+                CCMET_obj = CCMET(locations[loc][0], locations[loc][1], loc_info[i][2])
+
+                if DEBUG:
+                    pass_info[i]["cloud_cover"] = 0.0
+                else:
+                    pass_info[i]["cloud_cover"] = CCMET_obj.get_cloud_cover()
+
+            satellites[satellite][3][loc] = pass_info
 
     return satellites
+
+def date_table_generator(satellites_passes: dict, 
+                         min_elev: float = 50.0, 
+                         max_clouds: float = 100.0) -> dict:
+    
+    date_table = dict()
+    for satellite in satellites_passes:
+        for loc in satellites_passes[satellite][3]:
+            for pass_list in satellites_passes[satellite][3][loc]:
+                if pass_list["elevation"] >= min_elev and pass_list["cloud_cover"] <= max_clouds:
+                    # get the date as a string
+                    date = pass_list["UTC0_datetime"].split(" ")[0]
+                    if date not in date_table:
+                        date_table[date] = []
+                    dict_obj = pass_list
+                    dict_obj["satellite"] = satellite
+                    dict_obj["location"] = loc
+                    date_table[date].append(dict_obj)
+    
+    return date_table
+
+def date_table_to_markdown(date_table: dict) -> str:
+    return_str = ""
+
+    for date in date_table:
+        df_obj = pd.DataFrame(date_table[date])
+        df_obj = df_obj.sort_values(by=["UTC0_datetime"])
+        return_str += f"## {date}\n"
+        return_str += "Satellite | Location | UTC0 | Azimuth | Elevation | Cloud Cover\n"
+        return_str += "--- | --- | --- | --- | --- | ---\n"
+        for i in range(len(df_obj)):
+            clock_time = df_obj.iloc[i]["UTC0_datetime"].split(" ")[1]
+            return_str += f"{df_obj.iloc[i]['satellite']} | {df_obj.iloc[i]['location']} | {clock_time} | {df_obj.iloc[i]['azimuth']} | {df_obj.iloc[i]['elevation']} | {df_obj.iloc[i]['cloud_cover']}\n"
+        return_str += "\n\n"
+
+    return return_str
+
 
 if __name__ == "__main__":
     import argparse
@@ -82,7 +145,7 @@ if __name__ == "__main__":
         "--look_ahead_time",
         help="How many hours to look ahead",
         type=int,
-        default=24*3,
+        default=24*7,
     )
 
     args = parser.parse_args()
@@ -91,12 +154,24 @@ if __name__ == "__main__":
         DEBUG = True
         print("Debug mode activated")
 
+    if not DEBUG:
+        satellites = collect_TLEs(satellites) # Update TLEs
+    elif DEBUG and os.path.isfile("tle/satellites.json"):
+        with open("tle/satellites.json", "r") as f:
+            satellites = json.load(f)
+    else:
+        satellites = collect_TLEs(satellites)
+        with open("tle/satellites.json", "w") as f:
+            json.dump(satellites, f, indent=1)
+
+
     satellites_passes = compute_passes(satellites, locations, args.look_ahead_time)
 
-    for satellite in satellites_passes:
-        print(f"Next passes for {satellite}:")
-        for loc in satellites_passes[satellite][3]:
-            print(f"    {loc}:")
-            for pass_list in satellites_passes[satellite][3][loc]:
-                print(f"        {pass_list}")
+    date_table = date_table_generator(satellites_passes)
+
+    markdown_str = "# Satellite Forecast\n\n"
+    markdown_str += date_table_to_markdown(date_table)
+
+    with open("README.md", "w") as f:
+        f.write(markdown_str)
     
