fa9aec163ba18070cd55f96f4cbe07a3b5bbe289
sivertba <sivert.bakken@gmail.com>
2023-06-20 14:08:40 +0200

    Fix some stuff

--- pass_computer.py
+++ pass_computer.py
@@ -12,23 +12,24 @@ DEBUG = False
 
 file_dir = os.path.dirname(os.path.realpath(__file__))
 
+
 # Satellites as dict
 satellites = {
-    # "Landsat-7": [25682, "Line1", "Line2"],
-    # "Landsat-8": [39084, "Line1", "Line2"],
-    "HYPSO-1": [51053, "Line1", "Line2"],
-    "Sentinel-3A": [41335, "Line1", "Line2"],
-    "Sentinel-3B": [43437, "Line1", "Line2"],
-    "SENTINEL-2A": [40697, "Line1", "Line2"],
-    "SENTINEL-2B": [42063, "Line1", "Line2"],
+    # "Landsat-7": {"catnr": 25682, "line1": "Line1", "line2": "Line2"},
+    # "Landsat-8": {"catnr": 39084, "line1": "Line1", "line2": "Line2"},
+    "HYPSO-1": {"catnr": 51053, "line1": "Line1", "line2": "Line2", "min_elev": 45},
+    "Sentinel-3A": {"catnr": 41335, "line1": "Line1", "line2": "Line2", "min_elev": 90-68.5/2},
+    "Sentinel-3B": {"catnr": 43437, "line1": "Line1", "line2": "Line2", "min_elev": 90-68.5/2},
+    "SENTINEL-2A": {"catnr": 40697, "line1": "Line1", "line2": "Line2", "min_elev": 90-20},
+    "SENTINEL-2B": {"catnr": 42063, "line1": "Line1", "line2": "Line2", "min_elev": 90-20},
 }
 
 # Locations as dict
 locations = {
-    "Mjøsa": [60.70, 10.98, 0.0],
-    "Tyrifjorden": [60.03, 10.18, 0.0],
-    "Hemnessjøen": [59.68, 11.46, 0.0],
-    "Vansjø": [59.40, 10.82, 0.0],
+    "Mjøsa": {"lat": 60.70, "lon": 10.98, "alt": 0.0},
+    "Tyrifjorden": {"lat": 60.03, "lon": 10.18, "alt": 0.0},
+    "Hemnessjøen": {"lat": 59.68, "lon": 11.46, "alt": 0.0},
+    "Vansjø": {"lat": 59.40, "lon": 10.82, "alt": 0.0},
 }
 
 
@@ -46,11 +47,11 @@ def collect_TLEs(satellites: dict) -> dict:
         for satellite in satellites:
             if DEBUG:
                 print(f"collecting TLE for {satellite}")
-            url = f"https://celestrak.org/NORAD/elements/gp.php?CATNR={satellites[satellite][0]}&FORMAT=TLE"
+            url = f"https://celestrak.org/NORAD/elements/gp.php?CATNR={satellites[satellite]['catnr']}&FORMAT=TLE"
             tle = requests.get(url)
             tle = tle.text.splitlines()
-            satellites[satellite][1] = tle[1]
-            satellites[satellite][2] = tle[2]
+            satellites[satellite]['line1'] = tle[1]
+            satellites[satellite]['line2'] = tle[2]
     except:
         print('Error. TLE Update not successful')
     return satellites
@@ -75,26 +76,26 @@ def compute_passes(satellites: dict, locations: dict, look_ahead_time: int = 24*
         # Get orbital object from pyorbital using the TLEs
         sat_obj = Orbital(
             satellite,
-            line1=satellites[satellite][1],
-            line2=satellites[satellite][2]
+            line1=satellites[satellite]['line1'],
+            line2=satellites[satellite]['line2']
         )
 
-        satellites[satellite].append(dict())
+        satellites[satellite]['passes'] = dict()
         # Get next passes for each location
         for loc in locations:
             loc_info = sat_obj.get_next_passes(
                 datetime.utcnow(),
                 look_ahead_time,
-                locations[loc][1],
-                locations[loc][0],
-                locations[loc][2],
+                locations[loc]['lat'],
+                locations[loc]['lon'],
+                locations[loc]['alt'],
                 tol=0.001,
-                horizon=int( minimumElevation // 1)
+                horizon=int(minimumElevation // 1)
                 )
 
             # extract max elevation datetime and compute elevation
             pass_info = get_pass_info_list(locations, sat_obj, loc, loc_info)
-            satellites[satellite][3][loc] = pass_info
+            satellites[satellite]['passes'][loc] = pass_info
 
     return satellites
 
@@ -124,9 +125,9 @@ def get_pass_info_list(
                     "%Y-%m-%d %H:%M:%SZ")
 
         temp_obj = sat_obj.get_observer_look(loc_info[i][2],
-                                                     locations[loc][1],
-                                                     locations[loc][0],
-                                                     locations[loc][2])
+                                                     locations[loc]["lon"],
+                                                     locations[loc]["lat"],
+                                                     locations[loc]["alt"])
 
         # reduce to two decimals
         temp_obj = [round(temp_obj[0], 2), round(temp_obj[1], 2)]
@@ -138,7 +139,9 @@ def get_pass_info_list(
             pass_info[i]["cloud_cover"] = -1
         else:
             CCMET_obj = CCMET(
-                        locations[loc][0], locations[loc][1], loc_info[i][2])
+                        locations[loc]["lat"], 
+                        locations[loc]["lon"], 
+                        loc_info[i][2])
             pass_info[i]["cloud_cover"] = CCMET_obj.get_cloud_cover()
     
     return pass_info
@@ -162,16 +165,25 @@ def date_table_generator(satellites_passes: dict,
     """
     from pyorbital.orbital import astronomy
 
+    min_elev_static = min_elev
+
     date_table = dict()
     for satellite in satellites_passes:
-        for loc in satellites_passes[satellite][3]:
-            for pass_list in satellites_passes[satellite][3][loc]:
+        for loc in satellites_passes[satellite]["passes"]:
+            for pass_list in satellites_passes[satellite]["passes"][loc]:
                 # check sun zenith angle
                 sza = astronomy.sun_zenith_angle(pass_list["UTC0_datetime"],
-                                                 locations[loc][0],
-                                                 locations[loc][1])
+                                                 locations[loc]["lat"],
+                                                 locations[loc]["lon"])
                 if sza > 90.0:
                     continue
+
+                # check if min_elev key exists in satellite dict
+                if "min_elev" in satellites_passes[satellite]:
+                    min_elev = satellites_passes[satellite]["min_elev"]
+                else:
+                    min_elev = min_elev_static
+
                 if pass_list["elevation"] >= min_elev and pass_list["cloud_cover"] <= max_clouds:
                     # get the date as a string
                     date = pass_list["UTC0_datetime"].split(" ")[0]
@@ -237,7 +249,7 @@ def _get_cli_args():
         "--look_ahead_hrs",
         help="How many hours to look ahead",
         type=int,
-        default=24*7,
+        default=24*8,
     )
 
     parser.add_argument(
@@ -307,14 +319,22 @@ if __name__ == "__main__":
     markdown_str += "Location | Latitude | Longitude | Altitude\n"
     markdown_str += "--- | --- | --- | ---\n"
     for loc in locations:
-        markdown_str += f"{loc} | {locations[loc][0]} | {locations[loc][1]} | {locations[loc][2]}\n"
+        l0 = locations[loc]["lat"]
+        l1 = locations[loc]["lon"]
+        l2 = locations[loc]["alt"]
+        markdown_str += f"{loc} | {l0} | {l1} | {l2}\n"
 
     # add table of satellites
     markdown_str += "\n\n## Satellites\n\n"
     markdown_str += "Satellite | NORAD ID | Line 1 | Line 2\n"
     markdown_str += "--- | --- | --- | ---\n"
     for sat in satellites:
-        markdown_str += f"{sat} | {satellites[sat][0]} | {satellites[sat][1]} | {satellites[sat][2]}\n"
+        # markdown_str += f"{sat} | {satellites[sat][0]} | {satellites[sat][1]} | {satellites[sat][2]}\n"
+        sat_name = sat
+        norad_id = satellites[sat]["catnr"]
+        line1 = satellites[sat]["line1"]
+        line2 = satellites[sat]["line2"]
+        markdown_str += f"{sat_name} | {norad_id} | {line1} | {line2}\n"
 
     # convert markdown to html
     output = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n</head>\n<body>\n"
